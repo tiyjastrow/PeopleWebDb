9c2390a657ae6018d94aaf009fed34f7314c94b0
Matthew Rigsby <MatthewRigsby@Matthews-MacBook-Air.local>
2017-03-07 13:49:46 -0600

    init commit

---
+++ src/PeopleWeb.java
@@ -0,0 +1,4 @@
+public class PeopleWeb {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+/**
+ * Created by MatthewRigsby on 2/24/17.
+ */
+public class Person {
+
+    String idNumber;
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+    String ipAddress;
+
+    public Person(String idNumber, String firstName, String lastName, String email, String country, String ipAddress) {
+        this.idNumber = idNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+        this.ipAddress = ipAddress;
+    }
+}
